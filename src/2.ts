bdcbf38bb96b3c5ecc6780855136197e0f8464b6
Kirill <stevemcc.man@gmail.com>
2023-11-27 23:33:05 +0200

    i

--- src/2.ts
+++ src/2.ts
@@ -16,14 +16,8 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
+  constructor(public name: string, private department: string, protected salary: number) {
   }
 
   getEmployeeDetails() {
@@ -33,7 +27,14 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(public name: string, protected salary: number) {
+    super(name, 'manager', salary);
+    this.salary += 10000;
+  }
 }
+const m = new Manager('John', 1000);
+console.log(m);
+
 
 
 export {};
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,40 @@
+class Key {
+    private signature: string;
+    constructor() {
+        this.signature = new Date().getTime().toString(32);
+    }
+    getSignature():string {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key {
+        return key
+    }
+}
+
+abstract class House {
+    protected door = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+    comeIn(person: Person): void {
+        if (this.door) this.tenants.push(person);
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+    openDoor(key: Key) {
+        if(key.getSignature() === this.key.getSignature()) this.door = true
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -8,4 +45,10 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
+
+
+
+// Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false), і key, яка зберігає об'єкт класу Key. У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита. Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. Реалізуйте метод openDoor у цьому класі. Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
